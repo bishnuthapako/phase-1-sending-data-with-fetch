52451982190bb5f564c6c47181e9be9d58a8e533
Bishnu <bishnuthapako@gmail.com>
2022-05-08 20:04:24 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+
+function submitData(name, email){
+    let inputField = {
+        name: name,
+        email: email,
+    }
+
+    let configurationData = {
+        method: "POST",
+        headers:{
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(inputField)
+    }
+
+    const response = fetch(`http://localhost:3000/users`, configurationData)
+    .then(response => response.json())
+    .then((object) => {
+        let h2 = document.createElement('h2');
+        h2.innerHTML = object.id;
+        document.body.appendChild(h2);
+        console.log(object);
+    })
+    .catch(error=> {alert(error.message)          
+        document.body.append(error.message)});
+    return response
+
+}
